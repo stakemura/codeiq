12ed2f3057efbc3f66e8896f4cd71b3bc275bb70
stakemura <stakemura@gmail.com>
2013-10-13 08:11:53 +0900

    Added Q450 supplemental code

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-CodeIQ
+﻿CodeIQ
 ===================
-[CodeIQ](https://codeiq.jp/) Example Code
+[CodeIQ](https://codeiq.jp/) example code
 
-## Question
+## Questions
+
+### Monte-Carlo Simulation
 
 ### Combinational Optimization
+
+#### [Q450: Shortest common superstring](https://codeiq.jp/ace/stakemura/q450)
+
+* Code 1 : [shortest_superstring.py](shortest_superstring/shortest_superstring.py)
+  * Implemented in Python 2.7
+    * Depends on NetworkX and matplotlib
+  * This program is now a supplemental code not the answer code.
+  * Visualize the substring network or generate its file (See command line option).
 
 #### [Q408: Longest word chain](https://codeiq.jp/ace/stakemura/q408)
 
 * Code 1 : [word_chain.cpp](word_chain/word_chain.cpp)
   * Implemented in C++11
-    * Compiled with Visual Studio 2013 Preview (VC12)
+    * Compiled with Visual Studio 2013 RC (VC12)
   * This program can find <=32 word chain within about 1 second on modern PC.
   * As for 33, the execute time might be about 30 seconds.
   * However as for 34, the computation time will reach >100000 seconds !
@@ -18,7 +28,7 @@ CodeIQ
 
 * Code 2 : [word_chain_optimized.cpp](word_chain_optimized/word_chain_optimized.cpp)
   * Improved version
-  * This program can find 35(=longest) word chain in only 3 seconds on modern PC.
+  * This program can find 35(=longest) word chain in only 1-3 seconds on modern PC.
 
 ### Computational Geometry
 
---
+++ shortest_superstring/shortest_superstring.py
@@ -0,0 +1,107 @@
+﻿# -*- coding:utf-8 -*-
+
+"""
+@brief CodeIQ 'Shortest common superstring' problem ( https://codeiq.jp/ace/stakemura/q450 )
+@author Shintaro TAKEMURA (stakemura@gmail.com)
+
+This code is public domain, no warranty expressed or implied,
+Functionality is thought to be correct, but it's up to you to make
+sure it does what you want.
+"""
+
+import os
+import networkx as nx
+
+
+def calc_distance(s1, s2):
+    """
+    @rtype : int
+    """
+    length = len(s1)
+    for i in range(length):
+        if s2.startswith(s1[i:]):
+            return length - i
+    return 0
+
+
+def generate_network(parts):
+    """
+    @rtype : nx.DiGraph
+    """
+    G = nx.DiGraph()
+
+    for s1 in parts:
+        G.add_node(s1, weight=len(s1))
+
+    for s1 in parts:
+        for s2 in parts:
+            if s1 != s2:
+                d = calc_distance(s1, s2)
+                if d>0:
+                    G.add_edge(s1, s2, weight=d, label=s2[:d])
+    return G
+
+if __name__ == '__main__':
+    from optparse import OptionParser
+
+    parser = OptionParser()
+
+    parser.add_option("-o", "--output", dest="output", #default="superstring.graphml",
+                      help="output path", metavar="FILE")
+
+    options, args = parser.parse_args()
+
+    parts = ['onoult',
+             'oniosis',
+             'oultra',
+             'ilicov',
+             'canoco',
+             'tramic',
+             'oconio',
+             'pneumo',
+             'copics',
+             'icrosco',
+             'csilic',
+             'ovolcano']
+
+    G = generate_network(parts)
+
+    if options.output:
+        writers = {
+                'adjlist':nx.write_adjlist,
+                'dot':nx.write_dot,
+                'xdot':nx.write_dot,
+                'edges':nx.write_edgelist,
+                'elist':nx.write_edgelist,
+                'edgelist':nx.write_edgelist,
+                'weighted_edgelist':nx.write_weighted_edgelist,
+                'graphml':nx.write_graphml,
+                'gml':nx.write_gml,
+                'gpickle':nx.write_gpickle,
+                'pajek':nx.write_pajek,
+                'yaml':nx.write_yaml}
+
+        graph_type = os.path.splitext(options.output)[1][1:]
+        if graph_type in writers:
+            try:
+                writers[graph_type](G=G, path=options.output)
+            except Exception, inst:
+                print 'Graph write error:'
+                print inst
+        else:
+            raise Exception,'Unable to write graphs of type: '+str(graph_type)
+    else:
+        import matplotlib.pyplot as plt
+        pos = nx.circular_layout(G) # positions for all nodes
+        #pos = nx.spring_layout(G, iterations=20) # positions for all nodes
+        # nodes
+        nx.draw_networkx_nodes(G, pos)
+        # edges
+        nx.draw_networkx_edges(G, pos)
+        # node labels
+        nx.draw_networkx_labels(G, pos)
+        # edge labels
+        #nx.draw_networkx_edge_labels(G, pos)
+
+        plt.axis('off')
+        plt.show() # display
